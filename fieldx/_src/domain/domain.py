00c20a0d7f4fc368626ef8959ba16b3dbe066eb4
Juan Emmanuel Johnson <jemanjohnson34@gmail.com>
2023-10-31 19:19:01 +0100

    Initial Commit.

---
+++ fieldx/_src/domain/domain.py
@@ -0,0 +1,133 @@
+import typing as tp
+import equinox as eqx
+import jax.numpy as jnp
+import numpy as np
+# import jaxsw._src.domain.utils as d_utils
+from jaxtyping import Array
+from functools import reduce
+from operator import mul
+
+
+def check_inputs(x, name: str):
+    if isinstance(x, float) or isinstance(x, int):
+        return tuple([x])
+    elif isinstance(x, list):
+        return tuple(x)
+    elif isinstance(x, tuple):
+        return x
+    elif isinstance(x, jnp.ndarray) or isinstance(x, np.ndarray):
+        return tuple(map(lambda x: float(x), x))
+    raise ValueError(f"Unexpected type for {name}, got {value}.")
+
+
+class Domain(eqx.Module):
+    """Domain class for a rectangular domain
+
+    Attributes:
+        size (Tuple[int]): The size of the domain
+        xmin: (Iterable[float]): The min bounds for the input domain
+        xmax: (Iterable[float]): The max bounds for the input domain
+        coord (List[Array]): The coordinates of the domain
+        grid (Array): A grid of the domain
+        ndim (int): The number of dimenions of the domain
+        size (Tuple[int]): The size of each dimenions of the domain
+        cell_volume (float): The total volume of a grid cell
+    """
+
+    xmin: tp.Iterable[float] = eqx.static_field()
+    xmax: tp.Iterable[float] = eqx.static_field()
+    dx: tp.Iterable[float] = eqx.static_field()
+    Nx: tp.Iterable[int] = eqx.static_field()
+    Lx: tp.Iterable[float] = eqx.static_field()
+    ndim: int = eqx.static_field()
+
+    def __init__(
+        self,
+        xmin: tp.Union[float, tp.Iterable[float]],
+        xmax: tp.Union[float, tp.Iterable[float]],
+        dx: tp.Union[float, tp.Iterable[float]],
+        Nx: tp.Union[int, tp.Iterable[int]],
+        Lx: tp.Union[float, tp.Iterable[float]],
+    ):
+        xmin = check_inputs(xmin, name="xmin")
+        xmax = check_inputs(xmax, name="xmax")
+        dx = check_inputs(dx, name="dx")
+        Nx = check_inputs(Nx, name="Nx")
+        Lx = check_inputs(Lx, name="Lx")
+        assert len(xmin) == len(xmax) == len(dx) == len(Nx) == len(Lx)
+        self.xmin = xmin
+        self.xmax = xmax
+        self.dx = dx
+        self.Nx = Nx
+        self.Lx = Lx
+        self.ndim = len(self.xmin)
+
+    @property
+    def coords_axis(self) -> tp.List:
+        return list(map(d_utils.make_coords, self.xmin, self.xmax, self.Nx))
+
+    @property
+    def grid_axis(self) -> Array:
+        return d_utils.make_grid_from_coords(self.coords_axis)
+
+    @property
+    def coords(self) -> Array:
+        return jnp.asarray(d_utils.make_grid_coords(self.coords_axis))
+
+    @property
+    def cell_volume(self) -> float:
+        return reduce(mul, self.dx)
+
+    def __getitem__(self, values):
+        if isinstance(values, (jnp.ndarray, np.ndarray, tuple)):
+            values = list(values)
+        elif isinstance(values, slice):
+            values = list([values])
+
+        try:
+            iter(values)
+        except TypeError:
+            values = list(values)
+
+        if Ellipsis in values:
+            raise ValueError(f"Ellipsis not allowed. MUST be explicit.")
+
+        msg = f"Incompatible slice. MUST be explicit"
+        assert len(values) == len(self.coords_axis), msg
+
+        # get sliced coordinates
+        # sliced_coords = [jax.lax.slice(idx, [islice.start], [slice.stop], [islice.step]) for idx, islice in zip(self.coords_axis, values)]
+        sliced_coords = [idx[islice] for idx, islice in zip(self.coords_axis, values)]
+
+        # change Nx
+        Nx = list(map(lambda x: len(x), sliced_coords))
+
+        # change Lx
+        fn = lambda Lx, Nx, Ny: Lx * (Ny / Nx)
+        Lx = list(map(fn, self.Lx, self.Nx, Nx))
+
+        # change xmin, xmax
+        xmin = tuple(map(lambda x: float(x[0]), sliced_coords))
+        xmax = tuple(map(lambda x: float(x[-1]), sliced_coords))
+
+        return Domain(xmin=xmin, xmax=xmax, Lx=Lx, Nx=Nx, dx=self.dx)
+
+    def __mul__(self, other):
+        fn = lambda x, y: x + y
+
+        xmin = fn(self.xmin, other.xmin)
+        xmax = fn(self.xmax, other.xmax)
+        dx = fn(self.dx, other.dx)
+        Nx = fn(self.Nx, other.Nx)
+        Lx = fn(self.Lx, other.Lx)
+        return Domain(xmin=xmin, xmax=xmax, dx=dx, Nx=Nx, Lx=Lx)
+
+    def __rmult__(self, other):
+        fn = lambda x, y: y + x
+
+        xmin = fn(self.xmin, other.xmin)
+        xmax = fn(self.xmax, other.xmax)
+        dx = fn(self.dx, other.dx)
+        Nx = fn(self.Nx, other.Nx)
+        Lx = fn(self.Lx, other.Lx)
+        return Domain(xmin=xmin, xmax=xmax, dx=dx, Nx=Nx, Lx=Lx)
---
+++ fieldx/_src/domain/latlon.py
@@ -0,0 +1,108 @@
+import typing as tp
+
+import equinox as eqx
+import jax.numpy as jnp
+from jaxtyping import Array
+
+from fieldx._src.domain.domain import Domain
+from fieldx._src.utils.constants import R_EARTH
+from fieldx._src.utils.coriolis import beta_plane, coriolis_param
+
+
+class LatLonDomain(Domain):
+    f0: float = eqx.static_field()
+    beta: float = eqx.static_field()
+
+    def __init__(self, lat: Array, lon: Array):
+        # get latlon delts
+        dx, dy = lat_lon_deltas(lon=lon, lat=lat)
+
+        # get mean lat (for parameters)
+        lat0 = jnp.mean(lat)
+
+        xmin, ymin = lon.min(), lat.min()
+        xmax, ymax = lon.max(), lat.max()
+
+        self.xmin = (xmin, ymin)
+        self.xmax = (xmax, ymax)
+        self.f0 = coriolis_param(lat0)
+        self.beta = beta_plane(lat0)
+        self.dx = (dx, dy)
+
+    @property
+    def coords(self) -> tp.List:
+        def make_coords(xmin, xmax, delta):
+            return jnp.arange(xmin, xmax, delta)
+
+        return list(map(make_coords, self.xmin, self.xmax, self.dx))
+
+    @property
+    def dx_mean(self):
+        return jnp.mean(jnp.asarray(self.dx))
+
+
+class LatLonMeanDomain(LatLonDomain):
+    def __init__(self, lat: Array, lon: Array):
+        # get latlon delts
+        dx, dy = lat_lon_deltas(lon=lon, lat=lat)
+
+        # get mean lat (for parameters)
+        lat0 = jnp.mean(lat)
+
+        dx, dy = jnp.mean(dx), jnp.mean(dy)
+
+        xmin, ymin = 0.0, 0.0
+        xmax, ymax = dx * lon.size, dy * lat.size
+
+        self.xmin = (xmin, ymin)
+        self.xmax = (xmax, ymax)
+        self.f0 = coriolis_param(lat0)
+        self.beta = beta_plane(lat0)
+        self.dx = (dx, dy)
+
+
+def lat_lon_deltas(
+    lon: Array, lat: Array, radius: float = R_EARTH
+) -> tp.Tuple[Array, Array]:
+    """Calculates the dx,dy for lon/lat coordinates. Uses
+    the spherical Earth projected onto a plane approx.
+
+    Eqn:
+        d = R √ [Δϕ² + cos(ϕₘ)Δλ]
+
+        Δϕ - change in latitude
+        Δλ - change in longitude
+        ϕₘ - mean latitude
+        R - radius of the Earth
+
+    Args:
+        lon (Array): the longitude coordinates [degrees]
+        lat (Array): the latitude coordinates [degrees]
+
+    Returns:
+        dx (Array): the change in x [m]
+        dy (Array): the change in y [m]
+
+    Resources:
+        https://en.wikipedia.org/wiki/Geographical_distance#Spherical_Earth_projected_to_a_plane
+
+    """
+
+    assert lon.ndim == lat.ndim
+    assert lon.ndim > 0 and lon.ndim < 3
+
+    if lon.ndim < 2:
+        lon, lat = jnp.meshgrid(lon, lat, indexing="ij")
+
+    lon = jnp.deg2rad(lon)
+    lat = jnp.deg2rad(lat)
+
+    lat_mean = jnp.mean(lat)
+
+    dlon_dx, dlon_dy = jnp.gradient(lon)
+    dlat_dx, dlat_dy = jnp.gradient(lat)
+
+    dx = radius * jnp.hypot(dlat_dx, dlon_dx * jnp.cos(lat_mean))
+    dy = radius * jnp.hypot(dlat_dy, dlon_dy * jnp.cos(lat_mean))
+
+    return dx, dy
